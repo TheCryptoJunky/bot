a33c035abbf4d358b3abe86a490ef6b0f749ca92
TheCryptoJunky <thecryptojunky@gmail.com>
2024-10-22 23:04:27 -0400

    factoring

--- src/rl_agent/wallet_swarm.py
+++ src/rl_agent/wallet_swarm.py
@@ -1,21 +1,116 @@
 # wallet_swarm.py
-import logging
-import wallet
+from .manual_overrides import ManualOverrides
+from .goal_adjuster import GoalAdjuster
+from typing import List, Dict
+import os
 
 class WalletSwarm:
-    def __init__(self, config):
-        self.config = config
-        self.wallets = []
+    """
+    A class to manage a swarm of wallets.
+    """
+    def __init__(self, wallet_dir: str, manual_overrides: ManualOverrides, goal_adjuster: GoalAdjuster):
+        """
+        Initialize the wallet swarm.
 
-    def add_wallet(self, wallet):
-        self.wallets.append(wallet)
+        Args:
+            wallet_dir (str): The directory where wallets are stored.
+            manual_overrides (ManualOverrides): An instance of ManualOverrides.
+            goal_adjuster (GoalAdjuster): An instance of GoalAdjuster.
+        """
+        self.wallet_dir = wallet_dir
+        self.manual_overrides = manual_overrides
+        self.goal_adjuster = goal_adjuster
+        self.wallets = {}
 
-    def remove_wallet(self, wallet):
-        self.wallets.remove(wallet)
+    def allocate_wallets(self, num_wallets: int) -> List[str]:
+        """
+        Allocate wallets based on the goal adjuster's strategy.
 
-    def get_wallets(self):
-        return self.wallets
+        Args:
+            num_wallets (int): The number of wallets to allocate.
 
-# Usage:
-wallet_swarm = WalletSwarm(config)
-wallet_swarm.add_wallet(wallet)
+        Returns:
+            List[str]: A list of allocated wallet names.
+        """
+        wallets = self.goal_adjuster.allocate_wallets(num_wallets)
+        for wallet in wallets:
+            self.create_wallet(wallet)
+        return wallets
+
+    def manage_wallets(self) -> None:
+        """
+        Manage wallets based on manual overrides and the goal adjuster's strategy.
+        """
+        self.manual_overrides.apply_overrides(self.wallets)
+        self.goal_adjuster.adjust_wallets(self.wallets)
+
+    def create_wallet(self, wallet_name: str) -> None:
+        """
+        Create a new wallet.
+
+        Args:
+            wallet_name (str): The name of the wallet to create.
+        """
+        wallet_path = os.path.join(self.wallet_dir, wallet_name)
+        with open(wallet_path, 'w') as f:
+            f.write('')
+        self.wallets[wallet_name] = wallet_path
+
+    def delete_wallet(self, wallet_name: str) -> None:
+        """
+        Delete a wallet.
+
+        Args:
+            wallet_name (str): The name of the wallet to delete.
+        """
+        if wallet_name in self.wallets:
+            os.remove(self.wallets[wallet_name])
+            del self.wallets[wallet_name]
+
+    def get_wallet(self, wallet_name: str) -> str:
+        """
+        Get a wallet's path.
+
+        Args:
+            wallet_name (str): The name of the wallet to get.
+
+        Returns:
+            str: The path of the wallet.
+        """
+        return self.wallets.get(wallet_name)
+
+    def update_wallet(self, wallet_name: str, new_name: str) -> None:
+        """
+        Update a wallet's name.
+
+        Args:
+            wallet_name (str): The current name of the wallet.
+            new_name (str): The new name of the wallet.
+        """
+        if wallet_name in self.wallets:
+            old_path = self.wallets[wallet_name]
+            new_path = os.path.join(self.wallet_dir, new_name)
+            os.rename(old_path, new_path)
+            self.wallets[new_name] = new_path
+            del self.wallets[wallet_name]
+
+    def save_wallet(self, wallet_name: str) -> None:
+        """
+        Save a wallet's state.
+
+        Args:
+            wallet_name (str): The name of the wallet to save.
+        """
+        # TO DO: implement wallet state saving
+
+    def remove_wallet_file(self, wallet_name: str) -> None:
+        """
+        Remove a wallet's file.
+
+        Args:
+            wallet_name (str): The name of the wallet to remove.
+        """
+        if wallet_name in self.wallets:
+            os.remove(self.wallets[wallet_name])
+
+# File path: /path/to/project/wallet_swarm.py
